2234e7ac982aa35be4c2865b66503e50f49b040e
Bosse Bandowski <bosse.bandowski@outlook.com>
2022-01-12 10:21:42 +0000

    small updated to cpu benchmark to match tpu benchmark

--- benchmarks/cpu_benchmark.py
+++ benchmarks/cpu_benchmark.py
@@ -15,38 +15,42 @@ MODEL_PATH = "../model_prep/models/model.pt"
 NUM_ROUNDS = 1
 NUM_IMGS = 10000
 
-# load model
-model = tf.keras.models.load_model(MODEL_PATH)
-# load dataset
-(_, _), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+def main():
+    # load model
+    model = tf.keras.models.load_model(MODEL_PATH)
+    # load dataset
+    (_, _), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
 
-# define columns of output frame
-columns = ["round", "img_id", "label", "prediction", "time"]
-output_frame = pd.DataFrame(columns=columns, index=range(0, NUM_ROUNDS * NUM_IMGS))
-# do 30 times
-row_idx = 0
-for round in range(NUM_ROUNDS):
-    print(f"round {round}/{NUM_ROUNDS}")
-    # iterate over the entire dataset
-    for idx in range(NUM_IMGS): 
-        # reformat input image to batch of size 1
-        input_img = np.asarray([test_images[idx]])
-        # start timer
-        t0 = time.time()
-        # run inference
-        result = model(input_img)
-        # stop timer
-        t1 = time.time()
-        # fill output
-        output_frame.loc[row_idx, "round"] = round
-        output_frame.loc[row_idx, "img_id"] = idx
-        output_frame.loc[row_idx, "label"] = test_labels[idx][0]
-        output_frame.loc[row_idx, "prediction"] = np.argmax(result)
-        output_frame.loc[row_idx, "time"] = t1 - t0
-        # increment row idx
-        row_idx += 1
+    # define columns of output frame
+    columns = ["round", "img_id", "label", "prediction", "time_gross", "time_net"]
+    output_frame = pd.DataFrame(columns=columns, index=range(0, NUM_ROUNDS * NUM_IMGS))
 
-print(output_frame.head())
-print(output_frame.tail())
+    row_idx = 0
+    for round in range(NUM_ROUNDS):
+        print(f"round {round}/{NUM_ROUNDS}")
+        # iterate over the entire dataset
+        for idx in range(NUM_IMGS): 
+            # reformat input image to batch of size 1
+            input_img = np.asarray([test_images[idx]])
+            # start timer
+            t0 = time.time()
+            # run inference
+            result = model(input_img)
+            # stop timer
+            t1 = time.time()
+            # fill output
+            output_frame.loc[row_idx, "round"] = round
+            output_frame.loc[row_idx, "img_id"] = idx
+            output_frame.loc[row_idx, "label"] = test_labels[idx][0]
+            output_frame.loc[row_idx, "prediction"] = np.argmax(result)
+            output_frame.loc[row_idx, "time_net"] = t1 - t0
+            # increment row idx
+            row_idx += 1
 
-output_frame.to_csv('results/cpu_benchmark.csv')
+    print(output_frame.head())
+    print(output_frame.tail())
+
+    output_frame.to_csv('results/cpu_benchmark.csv')
+
+if __name__ == "__main__":
+    main()
